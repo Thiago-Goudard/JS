1933e1f96ad28da266f8468bb55e1d10f9418a52
Thiago Thierry Goudard <thiago_goudard@estudante.sesisenai.org.br>
2025-03-21 10:07:29 -0300

    atividade ate a 25

--- script.js
+++ script.js
@@ -33,11 +33,146 @@ while(i > 0){
     console.log('nuemero '+(i--))
 }*/
 
-const text = prompt('Escreva uma pavavra');
+//atividade 4
+/*const text = prompt('Escreva uma pavavra');
 function inverterTexto(text){
     return text.split('').reverse().join('');
 
 }
-const inverter = inverterTexto(text)
+const inverter = inverterTexto(text);
 
-console.log(inverter);
+console.log(inverter);*/
+
+//atividade 5
+/*const text = prompt('Escreva a palavra, e vou dizer quantos cacteres tem');
+function contarCaractere(text){
+    console.log(text.length);
+}
+
+contarCaractere(text);*/
+
+//atividade 6
+/*const marca1 = prompt('Escreva o marca do seu carro');
+const modelo1 = prompt('Escreva o modelo do seu carro');
+const ano1 = prompt('Escreva o ano do seu carro');
+const carro = {
+    marca: (marca1),
+    modelo:(modelo1),
+    ano:(ano1),
+}
+
+console.log(carro);*/
+
+//atividade 7
+/*const nome = prompt('Escreva seu nome, piá');  
+
+function mensagemPersonalizada(nome) {  
+    const ola = 'Olá '; 
+    console.log(ola+nome);  
+}  
+
+mensagemPersonalizada(nome);
+
+//ativade 8
+
+/*const num1 = parseFloat(prompt('Escreva 1 numero'));
+const num2 = parseFloat(prompt('Escreva 1 numero'));
+const num3 = parseFloat(prompt('Escreva 1 numero'));
+function media(){
+    console.log  ((num1+num2+num3)/3);
+  
+}
+media();*/
+
+//atividade 9
+/*for (let i = 1; i <= 20; i++) {  
+    if (i % 3 === 0) {  
+        console.log(i + ": multiplo de 3");  
+    } else {  
+        console.log(i + ": Ñ multiplo de 3");  
+    }  
+} */
+
+//atidade 10
+/*const nome = prompt('Escreva uma palavra');  
+
+function verificarPalindromo() {  
+    const rev = nome.split('').reverse().join('');  
+    if (nome === rev) { 
+        console.log('É palíndromo');  
+    } else {  
+        console.log('Não é palíndromo');  
+    }  
+}  
+
+verificarPalindromo();*/
+        
+//atividade 11
+/*const numero = prompt('digite um numero');
+
+function verificarSinal (){
+
+
+    if (numero > 0); {  
+        console.log('Positivo');  
+    } else if (numeroo < 0){  
+        console.log('numero negativo');  
+    }  else{
+        console.log('0');
+    }
+
+}
+verificarSinal();
+
+
+
+
+*/
+
+
+//atividade 12
+/*const temperatura = parseFloat(prompt('digite uma temperatura'));
+
+function converterTemperatura ();{
+
+    console.log((temperatura*1.8)+32);
+
+}
+
+converterTemperatura();*/   
+
+//atividade 13
+/*let soma = 0;
+let i = 1;
+
+while(i<=100){
+    soma += i;
+    i++;
+}
+console.log('a soma é '+ soma);*/
+
+//atividade 14
+/*let texto = prompt ('Digite seu texto:');
+function substituirPalavra(text){
+    console.log(text.replace(/azul/g, "vermelho"))
+}
+substituirPalavra(texto)*/
+
+//atividade 25
+/*function anoBissexto(ano) {  
+
+    if ((ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0)) {  
+        return true;   
+    } else {  
+        return false; 
+    }  
+}  
+  
+console.log(anoBissexto(2020));  
+console.log(anoBissexto(1900));  
+console.log(anoBissexto(2000));  
+console.log(anoBissexto(2023));*/ 
+
+    
+
+
